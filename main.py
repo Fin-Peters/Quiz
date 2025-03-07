ae98d018345762f4c673904b6b45903d8c7fb558
FIN <fpeters@student.saintaug.nsw.edu.au>
2025-03-07 13:23:08 +1100

    made simpler

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import CombinedQuiz
+catagory_ans = ''
 
-
-catagory_ans = input('What catagory would you like to play? (history, science, math, or random)')
-
-if catagory_ans == 'history':
-    import history
+def intro():
+    catagory_ans = input('What catagory would you like to play? (history, science, math, or random)')
+    pass#intro and catatgory selection
 
 
 def main():
