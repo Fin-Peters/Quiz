c4ff383dbfd482d87795a794bab387ae3eae03d2
FIN <fpeters@student.saintaug.nsw.edu.au>
2025-03-07 12:41:41 +1100

    decided on catagories

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
 
+catagory_ans = input('What catagory would you like to play? (history, science, math, or random)')
+
+if catagory_ans == 'history':
+    import history
+
 
 def main():
-    
+
     pass
