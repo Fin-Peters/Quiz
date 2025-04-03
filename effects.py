513bdbe001de3cf695064162232cb1b253b1908c
Fin Peters <fpeters@student.saintaug.nsw.edu.au>
2025-04-04 10:36:16 +1100

    removed testing code

--- effects.py
+++ effects.py
@@ -19,7 +19,7 @@ def intro(prompt_text):
             terminal.print(frame)
             sys.stdout.write("\033[K")
             sys.stdout.flush()
-            sleep(.000001)
+            sleep(0)
 
     print("")
 
